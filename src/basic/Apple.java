7ab20cd6f0eb86c6c21589ee4569283a21818f8a
google2050 <appleface2050@qq.com>
2016-02-29 22:34:42 +0800

    p160

---
+++ src/basic/Apple.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class Apple {
+	public String name;
+	public String color;
+	public double weight;
+
+	public Apple() {
+	}
+
+	public Apple(String name, String color) {
+		this.name = name;
+		this.color = color;
+	}
+
+	public Apple(String name, String color, double weight) {
+		this(name, color); // this用来调用重载重载的构造函数
+		this.weight = weight;
+	}
+
+}
